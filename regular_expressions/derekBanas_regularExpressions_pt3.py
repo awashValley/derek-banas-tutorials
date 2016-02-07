0d024df45d4079cfd16d90cd9906c2b9ef257aa3
awashValley <workuhm@Hailemichaels-MacBook-Pro.local>
2016-02-07 14:07:37 +0100

    add regular expressions projects

--- regular_expressions/derekBanas_regularExpressions_pt3.py
+++ regular_expressions/derekBanas_regularExpressions_pt3.py
@@ -7,3 +7,20 @@ strToSearch = ""
 for line in fl:
     strToSearch += line
     print(strToSearch)
+
+# pattFinder = re.compile('b3b2c', re.IGNORECASE)      # doesn't work
+# pattern = "b3b2c"
+# pattFinder = re.compile(pattern)
+
+## any letter
+pattFinder = re.compile('[a-z]', re.IGNORECASE)
+
+# findPatt = re.match(pattFinder, strToSearch)
+findPatt = re.findall(pattFinder, strToSearch)
+
+if(findPatt):
+    # print(findPatt.group())
+    for i in findPatt:
+        print(i)
+else:
+    print("Nothing Found")
